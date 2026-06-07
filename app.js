3d761d246bd41d23ed98747e4dcd071b07f42df8
rdsemon <codewithemons@gmail.com>
2026-06-07 20:59:19 +0600

    feat(app): add userRouter

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const pizzaRoute = require("./router/pizzaRoute");
+const userRouter = require("./router/userRoute");
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/images", express.static("dev-data/images"));
 
 app.use(`/api/v1/pizzas`, pizzaRoute);
+app.use("/api/v1/user", userRouter);
 
 module.exports = app;
